e2a8c6236c27661c1b3cf301ad9a83f073c7e1e6
Danilo Kallan <danilokallan@hotmail.com>
2019-04-05 10:13:59 +0200

    Created chunk from array above the game loop.

--- Engine.cpp
+++ Engine.cpp
@@ -88,18 +88,21 @@ int Engine::Run()
 	Cube cube = Cube(&cubeShader);
 	cube.Initialize();
 
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
+	const int chunkSize = 16 * 16 * 16;
+
+	glm::vec3 cubePositions[chunkSize];
+
+	for (int y = 0; y < 16; y++)
+	{
+		for (int z = 0; z < 16; z++)
+		{
+			for (int x = 0; x < 16; x++)
+			{
+				cubePositions[x + z * 16 + y * 16 * 16] = glm::vec3(x, y, z);
+			}
+		}
+	}
+
 
 	cube.AddTexture("Resources/Assets/texture_stone.png");
 
@@ -123,13 +126,11 @@ int Engine::Run()
 		glm::mat4 view = Camera::Instance().GetViewMatrix();
 		cubeShader.SetMat4("view", view);
 
-		for (unsigned int i = 0; i < 10; i++)
+		for (unsigned int i = 0; i < chunkSize; i++)
 		{
 			// calculate the model matrix for each object and pass it to shader before drawing
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			cubeShader.SetMat4("model", model);
 
 			cube.Draw();
